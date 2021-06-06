9c407635b0f89f188230a6c55235d5875f98eaba
Diogo Torres <diogo.torres93@gmail.com>
2021-06-06 17:30:23 -0300

    dificuldade dinamica

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -1,6 +1,8 @@
 import time
 
 from backend.util.crypto_hash import crypto_hash
+from backend.config import MINE_RATE
+
 
 GENESIS_DATA = {
     'timestamp': 1,
@@ -26,6 +28,7 @@ class Block:
         self.difficulty = difficulty
         self.nonce = nonce
 
+    
     def __repr__(self):
         return (
             'Block('
@@ -37,6 +40,7 @@ class Block:
             f'nonce: {self.nonce})'
         )
 
+    
     @staticmethod
     def mine_block(last_block,  data):
         """
@@ -45,17 +49,19 @@ class Block:
         """
         timestamp = time.time_ns()
         last_hash = last_block.hash
-        difficulty = last_block.difficulty
+        difficulty = Block.adjust_difficulty(last_block, timestamp)
         nonce = 0
         hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
         while hash[0:difficulty] != '0' * difficulty:
             nonce += 1
             timestamp = time.time_ns()
+            difficulty = Block.adjust_difficulty(last_block, timestamp)
             hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
-            
+
         return Block(timestamp, last_hash, hash, data, difficulty, nonce)
 
+    
     @staticmethod
     def genesis():
         """
@@ -72,6 +78,23 @@ class Block:
         # funciona apenas se os labels do dicionario forem identicos aos
         # parametros do metodo
         return Block(**GENESIS_DATA)
+
+    
+    @staticmethod
+    def adjust_difficulty(last_block, new_timestamp):
+        """
+        Calculate the adjusted difficulty according to the MINE_RATE
+        Increase the difficulty for quickly mine blocks
+        Decrease the difficulty for slowly mined blocks
+        """
+        if (new_timestamp - last_block.timestamp) < MINE_RATE:
+            return last_block.difficulty + 1
+
+        if (last_block.difficulty - 1) > 0:
+            return last_block.difficulty - 1
+
+        return 1
+
 
 
 def main():
